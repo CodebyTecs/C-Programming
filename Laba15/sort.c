50e8926611e4cb3c4abfc774059f31a56d398697
codebytecs <va.pogoreloff@gmail.com>
2025-03-04 14:15:28 +0700

    create sort.c

---
+++ Laba15/sort.c
@@ -0,0 +1,15 @@
+void SelectSort(struct Student students[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) { 
+            if (students[j].total < students[minIndex].total) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            struct Student temp = students[i];
+            students[i] = students[minIndex];
+            students[minIndex] = temp;
+        }
+    }
+}
