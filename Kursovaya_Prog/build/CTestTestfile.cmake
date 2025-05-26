7332badde11ed564c89a99314d978f34f14e1eef
Tecs <va.pogoreloff@gmail.com>
2025-05-26 13:50:19 +0700

    Delete Kursovaya_Prog/build directory

--- Kursovaya_Prog/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tecs/Documents/Codding/Kursovaya_Prog
-# Build directory: /Users/tecs/Documents/Codding/Kursovaya_Prog/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(FunctionsTest "/Users/tecs/Documents/Codding/Kursovaya_Prog/build/test_functions")
-set_tests_properties(FunctionsTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/tecs/Documents/Codding/Kursovaya_Prog/CMakeLists.txt;34;add_test;/Users/tecs/Documents/Codding/Kursovaya_Prog/CMakeLists.txt;0;")
